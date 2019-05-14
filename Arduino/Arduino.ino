f5c45577414d6270e6092f7b16e480fdc1656c7f
RaphaelSanti31 <gani.ipek69@gmail.com>
2019-05-15 00:45:12 +0200

    Araba

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,44 @@
+class gamescore {
+  public:
+    score = 0;
+    scored = False;
+    finish = 8;
+  int check_signal(state) {
+    
+      if (state == False and not scored){
+        score = score + 1;
+        scored = True;
+      }
+      
+      if else (scored and a == True){
+        scored = False;
+      }
+
+      if (score == finish) {
+        GameEnde(True);
+      }
+  }
+    
+}
+
+int GameEnde (signal){
+  if(signal == True){
+    Havayı kapat ==
+  }
+  
+}
+
+int Reset (){
+  
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
